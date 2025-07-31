925273321e6e624714053bc5dab5d241d312f68f
RifanMuhammed <rifanmuhammed2028@cs.sjcetpalai.ac.in>
2025-07-31 11:43:18 +0530

    Update Queue(OP).c

--- Queue(OP).c
+++ Queue(OP).c
@@ -1,39 +1,13 @@
 #include <stdio.h>
 #define MAX 5
 
-int queue[MAX];
-int front = -1, rear = -1;
+int queue[MAX] = {11, 22, 33};
+int front = 0;   
+int rear = 2;    
 
-// Function to insert element into queue
-void enqueue(int value) {
-    if (rear == MAX - 1) {
-        printf("Queue is full (Overflow)\n");
-    } else {
-        if (front == -1) front = 0; // First insertion
-        rear++;
-        queue[rear] = value;
-        printf("%d inserted into the queue\n", value);
-    }
-}
-
-// Function to delete element from queue
-void dequeue() {
-    if (front == -1 || front > rear) {
-        printf("Queue is empty (Underflow)\n");
-    } else {
-        printf("Deleted: %d\n", queue[front]);
-        front++;
-        if (front > rear) {
-            // Reset queue when all elements are removed
-            front = rear = -1;
-        }
-    }
-}
-
-// Function to display queue elements
 void display() {
     if (front == -1) {
-        printf("Queue is empty\n");
+        printf("");
     } else {
         printf("Queue: ");
         for (int i = front; i <= rear; i++) {
@@ -43,20 +17,50 @@ void display() {
     }
 }
 
-// Main function
-int main() {
-    enqueue(10);
-    enqueue(20);
-    enqueue(30);
+void enqueue(int value) {
+    if (rear == MAX - 1) {
+        printf("Queue is full (Overflow)\n");
+    } else {
+        if (front == -1) {  
+            front = 0;
+        }
+        rear++;
+        queue[rear] = value;
+        printf("%d inserted into the queue\n", value);
+    }
     display();
+}
+
+void dequeue() {
+    if (front == -1) {
+        printf("Queue is empty (Underflow)\n");
+    } else {
+        printf("Deleted: %d\n", queue[front]);
+        if (front == rear) {
+            front = -1;
+            rear = -1;
+        } else {
+            front++;
+        }
+    }
+    display();
+}
+
+int main() {
+    display();
+
+    enqueue(44);
+    enqueue(55);
+    enqueue(66);  
 
     dequeue();
-    display();
+    dequeue();
+    dequeue();
+    dequeue();
+    dequeue();
+    dequeue();   
 
-    enqueue(40);
-    enqueue(50);
-    enqueue(60);  // This will show overflow
-    display();
+    enqueue(77);
 
     return 0;
 }
